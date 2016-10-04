b0fe562a02bc35362eb9ab65a64f5ef4ec8cedc3
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2016-10-04 16:09:19 -0700

    formatting

--- modules/formatter.js
+++ modules/formatter.js
@@ -2,7 +2,7 @@
 
 let numeral = require("numeral");
 
-let color = "#009cdb";
+let color = "#84BF41";
 
 exports.formatProperties = properties => {
 
@@ -11,9 +11,10 @@ exports.formatProperties = properties => {
         properties.forEach(property => {
             let fields = [];
             fields.push({title: "Address", value: `${property.get("Address__c")}, ${property.get("City__c")} ${property.get("State__c")}`, short:true});
-            fields.push({title: "Link", value: "https://login.salesforce.com/" + property.getId(), short:true});
-            fields.push({title: "Bedrooms", value: property.get("Beds__c"), short:true});
             fields.push({title: "Price", value: `${numeral(property.get("Price__c")).format('$0,0')}`, short:true});
+            fields.push({title: "Bedrooms", value: property.get("Beds__c"), short:true});
+            fields.push({title: "Bathrooms", value: property.get("Baths__c"), short:true});
+            fields.push({title: "Open in Salesforce", value: "https://login.salesforce.com/" + property.getId(), short:true});
             attachments.push({color: color, fields: fields});
         });
         return attachments;
